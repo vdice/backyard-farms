034d8d923653540451a962c8b10b5aaac1a1fe9a
Travis Johnston & Vaughn Dice <student@epicodus.com>
2015-10-05 16:43:56 -0700

    clean up file-upload component logic

--- app/components/file-upload.js
+++ app/components/file-upload.js
@@ -2,10 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions : {
-
     uploadFile(user) {
-      var preview = document.querySelector('img');
-      var file    = document.querySelector('input[type=file]').files[0];
+      var file    = document.querySelector('#img-upload').files[0];
       var reader  = new FileReader();
       var self = this
 
@@ -13,13 +11,7 @@ export default Ember.Component.extend({
         var base64Img = reader.result;
         self.sendAction('uploadFile', user, base64Img)
       }
-
-      if (file) {
-       reader.readAsDataURL(file);
-      } else {
-        preview.src = "";
-      }
-
+      reader.readAsDataURL(file);
     }
   }
 });
