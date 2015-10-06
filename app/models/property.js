294a696eee9c72eef0bef7d055000a1166bbf0e0
travis crampton & jake glassman <student@epicodus.com>
2015-10-06 09:33:18 -0700

    add location route and created location-tile along with city-tile for homepage

--- app/models/property.js
+++ app/models/property.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  location: DS.attr(),
+  location: DS.belongsTo('location', {async: true}),
   type: DS.attr(),
   user: DS.belongsTo('user', {async:true}),
   dates: DS.attr(),
